d18177a78cc15153ce230b17a74de02b5c46e548
wisestMe <35577633+wisestme@users.noreply.github.com>
2018-06-29 08:10:21 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,6 +1,6 @@
 // Size and color input
-
 let height, width, color;
+
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function(event){
 	event.preventDefault();
@@ -8,17 +8,19 @@ $('#sizePicker').submit(function(event){
 	width = $('#inputWidth').val();
 	makeGrid(height, width)
 });
+
 // function to clear and create grid dynamically using "for" and "while" loops
 function makeGrid(row, col) {
 	$('tr').remove();
 	for(var i = 1; i <= row; i++){
 		$('#pixelCanvas').append('<tr></tr>');
-		let j = 1
+		let j = 1;
 		while (j <= col){
 			$('tr').filter(":last").append('<td></td>');
-			j++
+			j++;
 		}
 	}
+	
 	//Event listeners to modify grid colors
 	$('td').click(function addColor(){
 		color = $('#colorPicker').val();
